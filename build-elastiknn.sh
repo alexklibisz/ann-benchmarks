c09055abf7a2ffefa73ee9e10f6427757d604da2
Alex Klibisz <aklibisz@gmail.com>
2019-12-28 10:54:43 -0500

    Some progress calling eknn model

--- build-elastiknn.sh
+++ build-elastiknn.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -e
 
-ANN_DIR=$(pwd)
-EKN_DIR=$HOME/Documents/dev/elastiknn
-
-cd $EKN_DIR && ./gradlew clean assemble 
-cp $EKN_DIR/es74x/build/distributions/elastiknn-0.0.0-SNAPSHOT_es7.4.0.zip $ANN_DIR/elastiknn.zip
-
-docker build -t elastiknn -f install/Dockerfile.elastiknn .
+docker build --no-cache -t ann-benchmarks-elastiknn -f install/Dockerfile.elastiknn .
 docker run -v $(pwd)/ann_benchmarks:/home/app/ann_benchmarks --rm elastiknn
--- install/entrypoint.elastiknn.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-service elasticsearch start
-python3 run_algorithm.py
