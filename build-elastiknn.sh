159a8c6b3eb5f1f5bcc9adcfb37599c239240064
Alex Klibisz <aklibisz@gmail.com>
2020-01-03 15:25:17 +0000

    Fix build script to work on GCP

--- build-elastiknn.sh
+++ build-elastiknn.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
+docker build -t ann-benchmarks -f install/Dockerfile .
 docker build --no-cache -t ann-benchmarks-elastiknn -f install/Dockerfile.elastiknn .
-docker run -v $(pwd)/ann_benchmarks:/home/app/ann_benchmarks --rm elastiknn
+docker run -v $(pwd)/ann_benchmarks:/home/app/ann_benchmarks --rm ann-benchmarks-elastiknn
