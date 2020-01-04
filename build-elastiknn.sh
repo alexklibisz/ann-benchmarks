04596ec2e94a13b3798637a01673c76f9e82cb25
Alex Klibisz <aklibisz@gmail.com>
2020-01-04 03:36:35 +0000

    More results for Jaccard

--- build-elastiknn.sh
+++ build-elastiknn.sh
@@ -2,5 +2,6 @@
 set -e
 
 docker build -t ann-benchmarks -f install/Dockerfile .
+docker build -t ann-benchmarks-datasketch -f install/Dockerfile.datasketch . 
 docker build --no-cache -t ann-benchmarks-elastiknn -f install/Dockerfile.elastiknn .
 docker run -v $(pwd)/ann_benchmarks:/home/app/ann_benchmarks --rm ann-benchmarks-elastiknn
