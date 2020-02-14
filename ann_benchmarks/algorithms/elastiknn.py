36603a1699c8b102ae34c1edb80a1f55ea6939dd
Alex Klibisz <aklibisz@gmail.com>
2020-02-13 20:35:50 -0500

    comments in algos.yaml file, added use_cache as parameter

--- ann_benchmarks/algorithms/elastiknn.py
+++ ann_benchmarks/algorithms/elastiknn.py
@@ -6,14 +6,15 @@ from ann_benchmarks.algorithms.base import BaseANN
 
 class ElastiKnnExact(BaseANN):
 
-    def __init__(self, metric, n_shards=1, start_es: bool = False):
+    def __init__(self, metric, num_shards=1, use_cache: bool = True, start_es: bool = False):
         self._model = ElastiKnnModel(algorithm='exact', metric=metric)
         self.name = 'elastiknn-exact'
-        self.n_shards = n_shards
+        self.num_shards = num_shards
         self.batch_res = None
+        self.use_cache = use_cache
         self._dim = None
 
-        print(f"Running {self.name} with metric {metric} and {n_shards} shards")
+        print(f"Running {self.name} with metric {metric} and {num_shards} shards")
 
         if start_es:
             from subprocess import run
@@ -26,15 +27,15 @@ class ElastiKnnExact(BaseANN):
         if self._model._sim == SIMILARITY_JACCARD:
             self._dim = max(map(max, X)) + 1
             X = [SparseBoolVector(total_indices=self._dim, true_indices=x) for x in X]
-        return self._model.fit(X, recreate_index=True, shards=self.n_shards)
+        return self._model.fit(X, recreate_index=True, shards=self.num_shards)
 
     def query(self, q, n):
         if self._model._sim == SIMILARITY_JACCARD:
             q = SparseBoolVector(total_indices=self._dim, true_indices=q)
-        return self._model.kneighbors([q], n_neighbors=n, return_distance=False, use_cache=True)[0]
+        return self._model.kneighbors([q], n_neighbors=n, return_distance=False, use_cache=self.use_cache)[0]
 
     def batch_query(self, X, n):
-        self.batch_res = self._model.kneighbors(X, n_neighbors=n, return_distance=False, use_cache=True)
+        self.batch_res = self._model.kneighbors(X, n_neighbors=n, return_distance=False, use_cache=self.use_cache)
 
     def get_batch_results(self):
         return self.batch_res
@@ -42,13 +43,14 @@ class ElastiKnnExact(BaseANN):
 
 class ElastiKnnLsh(BaseANN):
 
-    def __init__(self, metric, n_shards=1, num_tables: int = 1, num_bands: int = 10, num_rows: int = 2,
+    def __init__(self, metric, num_shards=1, num_tables: int = 1, num_bands: int = 10, num_rows: int = 2, use_cache: bool = True,
                  start_es: bool = False):
         self._model = ElastiKnnModel(algorithm='lsh', metric=metric,
                                      algorithm_params=dict(num_tables=num_tables, num_bands=num_bands, num_rows=num_rows))
         self.name = 'elastiknn-exact'
-        self.n_shards = n_shards
+        self.num_shards = num_shards
         self.batch_res = None
+        self.use_cache = use_cache
         self._dim = None
 
         if start_es:
@@ -62,15 +64,15 @@ class ElastiKnnLsh(BaseANN):
         if self._model._sim == SIMILARITY_JACCARD:
             self._dim = max(map(max, X)) + 1
             X = [SparseBoolVector(total_indices=self._dim, true_indices=x) for x in X]
-        return self._model.fit(X, recreate_index=True, shards=self.n_shards)
+        return self._model.fit(X, recreate_index=True, shards=self.num_shards)
 
     def query(self, q, n):
         if self._model._sim == SIMILARITY_JACCARD:
             q = SparseBoolVector(total_indices=self._dim, true_indices=q)
-        return self._model.kneighbors([q], n_neighbors=n, return_distance=False, use_cache=True)[0]
+        return self._model.kneighbors([q], n_neighbors=n, return_distance=False, use_cache=self.use_cache)[0]
 
     def batch_query(self, X, n):
-        self.batch_res = self._model.kneighbors(X, n_neighbors=n, return_distance=False, use_cache=True)
+        self.batch_res = self._model.kneighbors(X, n_neighbors=n, return_distance=False, use_cache=self.use_cache)
 
     def get_batch_results(self):
         return self.batch_res
