45f68013387d3de7655c675d3cb8f5da24ec3c77
Alex Klibisz <aklibisz@gmail.com>
2020-09-06 17:41:07 -0400

    Updated elastiknn dockerfile

--- build-elastiknn.sh
+++ build-elastiknn.sh
@@ -2,6 +2,5 @@
 set -e
 
 docker build -t ann-benchmarks -f install/Dockerfile .
-docker build -t ann-benchmarks-datasketch -f install/Dockerfile.datasketch . 
 docker build -t ann-benchmarks-elastiknn -f install/Dockerfile.elastiknn .
-docker run -v $(pwd)/ann_benchmarks:/home/app/ann_benchmarks --rm ann-benchmarks-elastiknn
+docker run -v $(pwd)/ann_benchmarks:/home/app/ann_benchmarks --rm ann-benchmarks-elastiknn --help
