b01bfcabec70c16642a31b2aef03878dfd10a8ad
Alex Klibisz <aklibisz@gmail.com>
2020-09-16 20:42:52 -0400

    more revert

--- build-elastiknn.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-
-docker build -t ann-benchmarks -f install/Dockerfile .
-docker build -t ann-benchmarks-elastiknn -f install/Dockerfile.elastiknn .
-docker run -v $(pwd)/ann_benchmarks:/home/app/ann_benchmarks --rm ann-benchmarks-elastiknn --help
